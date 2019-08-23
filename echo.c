d957673bec835974e3689e0c08c84db2591cce89
Hetav Panchani <hetav.panchani@students.iiit.ac.in>
2019-08-24 02:22:00 +0530

    echo will now trim whitespaces between arguments

--- echo.c
+++ echo.c
@@ -4,7 +4,7 @@
 
 void exec_echo(char * str) {  // to do : handle the case when commas are not closed
 
-    int flag_s_comma = 0, flag_d_comma = 0, flag_b_slash = 0;
+    int flag_s_comma = 0, flag_d_comma = 0, flag_b_slash = 0, flag_space = 1; // for trimming space
 
     for(int i=0; i < strlen(str); i++) {  // to do : what if echo \0advasdf ?
 
@@ -37,10 +37,18 @@ void exec_echo(char * str) {  // to do : handle the case when commas are not clo
                 flag_s_comma = 1;
             else if(str[i] == '"')
                 flag_d_comma = 1;
-            else if (str[i] == '\\')  // '\\' is '\' : extra backslash is to escape itself !
+            else if(str[i] == '\\')  // '\\' is '\' : extra backslash is to escape itself !
                 flag_b_slash = 1;
-            else 
+            else if(str[i] == ' ' && !flag_space) {
+                flag_space = 1;
                 printf("%c", str[i]);
+            }
+            else {
+                if(flag_space && str[i] == ' ') 
+                    continue;
+                flag_space = 0;
+                printf("%c", str[i]);
+            }
         }
     }
 
